6ee3a0df9a89d699c770d3a18bc99c86a9567e01
juanjocerero <elquefaltaba@gmail.com>
2017-10-09 16:39:18 +0200

    socket.io implemented

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,3 +1,4 @@
+// @flow
 
 import 'babel-polyfill'
 
@@ -6,14 +7,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { BrowserRouter } from 'react-router-dom'
 
-import App from './../shared/app'
-import helloReducer from './../shared/reducer/hello'
-import { APP_CONTAINER_SELECTOR } from './../shared/config'
-import { isProd } from './../shared/util'
+import setUpSocket from './socket'
+import App from '../shared/app'
+import helloReducer from '../shared/reducer/hello'
+import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import { isProd } from '../shared/util'
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
@@ -36,13 +38,15 @@ const wrapApp = (AppComponent, reduxStore) =>
     </BrowserRouter>
   </Provider>)
 
-ReactDOM.hydrate(wrapApp(App, store), rootEl)
+ReactDOM.render(wrapApp(App, store), rootEl)
 
 if (module.hot) {
   // flow-disable-next-line
-  module.hot.accept('./../shared/app', () => {
+  module.hot.accept('../shared/app', () => {
     // eslint-disable-next-line global-require
-    const NextApp = require('./../shared/app').default
+    const NextApp = require('../shared/app').default
     ReactDOM.render(wrapApp(NextApp, store), rootEl)
   })
 }
+
+setUpSocket(store)
